e4e1ae1fd678be9eb6766c4bfb88c68ef087a511
mayapony <1327909321@qq.com>
2023-01-13 12:39:05 +0800

    ✨ useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,97 @@
-import * as React from 'react';
+import * as React from "react";
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const List = (props) => {
+  console.log("List render...");
 
-function App() {
+  const list = props.list;
+  return (
+    <>
+      <h2>This List Components</h2>
+      <ul>
+        {list.map((item) => (
+          <Item item={item} key={item.objectID}></Item>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+const Item = (props) => {
+  console.log("Item render...");
+
+  const item = props.item;
+  return (
+    <li key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </li>
+  );
+};
+
+const Search = (props) => {
+  console.log("Search render...");
+
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setSearchTerm(event.target.value);
+  };
+
+  const handleBlur = (event) => {
+    console.log(event.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input
+        id="search"
+        type="text"
+        onChange={handleChange}
+        onBlur={handleBlur}
+      />
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
+    </div>
+  );
+};
+
+const App = () => {
+  console.log("App render...");
+
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+  let count = 0;
   return (
     <div>
       <h1>My Hacker Stories</h1>
       <Search />
       <hr />
-      <List />
+      <List list={stories} />
     </div>
   );
-}
-
-function List() {
-  return ( 
-    <>
-      <h2>This List Components</h2>
-      <ul>
-        {list.map(item => {
-          return (
-              <li key={item.objectID}>
-                <span>
-                  <a href={item.url}>{item.title}</a>
-                </span>
-                <span>{item.author}</span>
-                <span>{item.num_comments}</span>
-                <span>{item.points}</span>
-              </li>
-            );
-          })}
-      </ul>
-    </>
-  )
-}
-
-function Search() {
-  return (
-    <>
-      <label htmlFor='search'>Search: </label>
-      <input id='search' type='text' />
-    </>
-  )
-}
+};
 
 export default App;
